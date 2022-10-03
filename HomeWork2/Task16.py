005ea9330b2b2e36b282b2934a2c48fe8cc0961b
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-10-03 13:58:49 +0300

    corrections Task16

--- HomeWork2/Task16.py
+++ HomeWork2/Task16.py
@@ -1,11 +1,9 @@
 # 16). Задайте список из n чисел последовательности 〖(1+1/n)〗^n и выведите на экран их сумму.
 
-import math
-
 def create_list(num):
     new_list = []
     for i in range(1, num + 1):
-        new_list.append(round(((1 + 1 / i) ** i), 2))
+        new_list.append(round(((1 + 1 / i) ** i), 3))
     return(new_list)
 
 def sum_elements_list(user_list):
