07a8a7890197a8714496c34fc3310f53164f91b2
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-10-03 13:19:06 +0300

    Added Task15

---
+++ HomeWork2/Task15.py
@@ -0,0 +1,20 @@
+# 15). Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# *Пример:*
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def list_factorials(number):
+    list = []
+    factorial = 1
+    if number < 1:
+        list_factorials(int(input('Enter a positive number: ')))
+    else:
+        for i in range(1, number + 1):
+            factorial *= i
+            list.append(factorial)
+        print(list)
+
+try:
+    list_factorials(int(input('Enter a positive number: ')))
+except:
+    print('Error: Вы ввели не число!')
+    list_factorials(int(input('Enter a positive number: ')))
